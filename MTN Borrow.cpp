4174a3d3a7164a4a94cddde2a556df69241794c2
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2023-06-05 12:00:38 +0000

    Update MTN Borrow.cpp

--- MTN Borrow.cpp
+++ MTN Borrow.cpp
@@ -31,7 +31,7 @@ void listamount(){
 	cout << "How much do you want to borrow?\n";
 	
 	for (int num = 1; num <= 8; num++) {
-        for (double amount = 10.00; amount >= 1; --amount) {
+        for (double amount = 10.00; amount >= 1; amount-=10) {
             cout << num << ". GHS " << amount << endl;
             break;
             }
