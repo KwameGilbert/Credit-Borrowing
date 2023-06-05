5dfd191b0313e7a9497ef4d6ca91df3676ec4c4a
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2023-06-05 11:53:47 +0000

    Initial commit

---
+++ MTN Borrow.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void begin();
+void listamount();
+
+int main() {
+    begin();
+    listamount();
+    return 0;
+}
+
+void begin() {
+    string code;
+	for(;;){
+		cout << "Welcome to MTN Credit Borrow\n";
+    	cout << "Dial *506# to access menu: ";
+   		cin >> code;
+   		
+    	if (code == "*506#") {
+        	cout << "Menu accessed successfully.\n";
+        	break;
+       } 
+	   else {
+        cout << "You entered the wrong code.\n";
+    }
+}
+}
+
+void listamount(){
+	cout << "How much do you want to borrow?\n";
+	
+	for (int num = 1; num <= 8; num++) {
+        for (double amount = 10.00; amount >= 1; --amount) {
+            cout << num << ". GHS " << amount << endl;
+            break;
+            }
+   	}
+	}
