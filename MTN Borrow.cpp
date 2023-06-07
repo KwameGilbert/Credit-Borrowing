e3dd44a4edf7a05132f50dc6a5997495a2b38b1c
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2023-06-07 17:52:16 +0000

    *506#

--- MTN Borrow.cpp
+++ MTN Borrow.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 void begin();
 void listamount();
+void chooseamount();
+
 
 int main() {
   	begin();
     listamount();
+    void chooseamount();
     return 0;
 }
 
@@ -30,7 +33,7 @@ void begin() {
 
 void listamount() {
     cout << "How much do you want to borrow?\n";
-
+	
     double amnt = 10.00;  // Start with the maximum borrowing amount
 
     for (int num = 1; num <= 10; num++) {
@@ -40,5 +43,45 @@ void listamount() {
         cout << num << ". GHC " << amnt << endl;
         amnt--;  // Decrease the borrowing amount
     }
+    
 }
+
+ void chooseamount(){
+ 	int option;
+ 	double loan;
+ 	cin >> option;
+ 	
+ 	if (option==1){
+ 		loan=10.00;
+	 }
+	else if(option==2){
+	 	loan=9;
+	 }
+	else if(option==3){
+	 	loan=8;
+	 }
+	else if(option==4){
+	 	loan=7;
+	 }
+	else if(option==5){
+	 	loan=6;
+	 }
+	else if(option==6){
+	 	loan=5;
+	 }
+	else if(option==7){
+		loan=4;
+	 }
+	else if(option==8){
+		loan=3;
+	 }
+	else if(option==9){
+		loan=2;
+	 }
+	else if(option==10){
+		loan=1;
+	 }
+	 cout<< "You are borrowing" << loan;
+}
+ 
 	
